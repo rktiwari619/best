f1f1e883ac77f69a7154dfcbf4b9c1da376b0d3c
rktiwari619 <rkt.600@gmail.com>
2025-05-30 13:44:35 +0530

    sonar shared lib

---
+++ vars/sonar.groovy
@@ -0,0 +1,16 @@
+ stage("Sonarqube Analysis "){
+            steps{
+                withSonarQubeEnv('sonar-server') {
+                    sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=Petshop \
+                    -Dsonar.java.binaries=. \
+                    -Dsonar.projectKey=Petshop '''
+                }
+            }
+        }
+        stage("quality gate"){
+            steps {
+                script {
+                  waitForQualityGate abortPipeline: false, credentialsId: 'Sonar-token'
+                }
+           }
+        }
