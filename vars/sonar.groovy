7e3a95ec10cae5191eb6735e5c01afd7bf603d55
rktiwari619 <130204739+rktiwari619@users.noreply.github.com>
2025-05-30 20:46:32 +0530

    Update sonar.groovy

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -1,4 +1,3 @@
-
 def call(String status, String customMessage = '') {
     // Define Slack color codes based on build status
     def colorCode = '#439FE0' // Default: blue
@@ -13,19 +12,3 @@ def call(String status, String customMessage = '') {
 
     def defaultMsg = "${env.JOB_NAME} - Build #${env.BUILD_NUMBER} - ${status.toUpperCase()} \n ${env.BUILD_URL}"
 
-stage("Sonarqube Analysis "){
-            steps{
-                withSonarQubeEnv('sonar-server') {
-                    sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=Petshop \
-                    -Dsonar.java.binaries=. \
-                    -Dsonar.projectKey=Petshop '''
-                }
-            }
-        }
-        stage("quality gate"){
-            steps {
-                script {
-                  waitForQualityGate abortPipeline: false, credentialsId: 'Sonar-token'
-                }
-           }
-        }
